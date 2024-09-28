63e4d5c327df21503e2b9f69418e87b59ea58d89
Pradeep Kadiyala <pradeepkadiyala@pradeep-mbp.lan>
2024-09-28 17:01:25 -0500

    feat: simplified the API with fewshots

--- router.py
+++ router.py
@@ -1,27 +1,15 @@
 from fastapi import APIRouter
-from entities.CustomAgent import CustomAgent
 from typing import Any
 
-from service import chat_with_agent, chat_custom_agent
+#from service import chat_with_agent, chat_custom_agent
+from service import chat_with_fewshots
 router = APIRouter()
-
-import json
 
 @router.get("/status", tags=["wm_agent"])
 async def wm():
     return [{"agent": "Hello the agent is active!"}]
 
-@router.post("/agent", tags=["chat_agent"])
-async def agent(query: str):
-    answer = chat_with_agent(query)
+@router.post("/agent/fewshots", tags=["fewshots"])
+async def agent_fewshots(query: str):
+    answer = chat_with_fewshots(query)
     return {"query": query, "answer": answer}
-
-@router.post("/agent/toocall")
-async def custom_agent(agent: CustomAgent) -> Any:
-    answer = chat_custom_agent(agent)
-    return {"answer": answer}
-
-@router.post("/agent/prompt")
-async def custom_agent(agent: CustomAgent) -> Any:
-    answer = chat_custom_agent(agent)
-    return {"answer": answer}
--- service.py
+++ service.py
@@ -1,48 +1,9 @@
-from pkjm.agents.agent_toolcall import AgentToolCall
-from pkjm.tools.retriever import LangChainRetriever
-from pkjm.tools.doc_loader import load_docs
-from pkjm.models.modelfactory import ModelFactory
-from entities.CustomAgent import CustomAgent
+from pkjm.agents.agent import AgentFactory
 
-from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, AIMessage
 
-from config import MODEL_CONFIG, DOCUMENTS, RETRIEVER_TOOL_CONFIG, PROMPT_CONFIG
-
-import json
-
-model_type = MODEL_CONFIG.pop("model_type")
-llm = ModelFactory.get_model(model_name=model_type, **MODEL_CONFIG)
-
-retriever = LangChainRetriever()    
-documents = load_docs(DOCUMENTS["file_path"])
-retriever.build_vectorstore(documents)
-retriever_tool = retriever.get_retriever_tool(
-        RETRIEVER_TOOL_CONFIG["name"], 
-        RETRIEVER_TOOL_CONFIG["description"]
-        )
-
-def chat_with_agent(query: str):
-    chat_history = []    
-    agentX = AgentToolCall(llm, [retriever_tool])    
-    human_msg = HumanMessage(content=query)
-    chat_history.append(human_msg)
-    agentX.createPrompt(PROMPT_CONFIG["system_message"])    
-    return call_agent(agentX)
-
-def chat_custom_agent(custom_agent: CustomAgent) -> any:
-    chat_history = []  
-    agentX = AgentToolCall(llm, [retriever_tool])        
-    prompt_message = custom_agent.prompt_template
-    human_msg = HumanMessage(content=custom_agent.query)
-    chat_history.append(human_msg)
-    agentX.createPrompt(prompt_message)    
-    return call_agent(agentX, chat_history=chat_history)
-
-def call_agent(agentX: AgentToolCall, chat_history: list):
-    agentX.createPrompt(PROMPT_CONFIG["system_message"])
-    response = agentX.createAgent().invoke({
-        "messages": chat_history        
+def chat_with_fewshots(query: str):
+    agent = AgentFactory.get_agent_fewshots_chat_prompt(query)
+    output = agent.invoke({
+        "input": query
     })
-    chat_history.append(AIMessage(content=response["output"]))
-    return response["output"]
+    return output
